23fb56615bafdc908d0cae7a9f99fc40ef36934b
Marko <marko_poikkimaki@hotmail.com>
2016-08-08 22:36:18 +0200

    serializable

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\AdvancedUserInterface;
  * @ORM\Table(name="yoda_user")
  * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
  */
-class User implements AdvancedUserInterface
+class User implements AdvancedUserInterface, \Serializable
 {
     /**
      * @var int
@@ -256,5 +256,34 @@ class User implements AdvancedUserInterface
     public function setEmail($email)
     {
         $this->email = $email;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     */
+    public function serialize()
+    {
+       return serialize(array(
+           $this->id,
+           $this->username,
+           $this->password
+       ));
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     */
+    public function unserialize($serialized)
+    {
+       list($this->id, $this->username, $this->password) = unserialize($serialized);
     }
 }
