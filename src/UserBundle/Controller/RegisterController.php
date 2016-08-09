27f1f392e7bdcb646462fb183a6b2bded9b4faca
Marko <marko_poikkimaki@hotmail.com>
2016-08-09 20:54:58 +0200

    Register page

---
+++ src/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marko
+ * Date: 09/08/2016
+ * Time: 20:36
+ */
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class RegisterController extends Controller {
+
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction(){
+        $form = $this->createFormBuilder()
+                ->add('username', 'text')
+                ->add('email', 'text')
+                ->add('password', 'password')
+                ->getForm();
+
+        return $this->render(':register:register.html.twig', array('form' => $form));
+    }
+
+} 
