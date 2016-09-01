7dc5008204c11abd21c1d735d4a119aeeb1d13ae
Marko <marko_poikkimaki@hotmail.com>
2016-09-01 21:13:19 +0200

    Twig extensions

---
+++ src/Yoda/EventBundle/Twig/EventExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Yoda\EventBundle\Twig;
+
+use Yoda\EventBundle\Util\DateUtil;
+
+class EventExtension extends \Twig_Extension {
+
+
+    public function getName()
+    {
+        return 'event';
+    }
+
+    public function getFilters()
+    {
+        return [
+           new \Twig_SimpleFilter('ago', [$this, 'calculateAgo'])
+        ];
+    }
+
+    public function calculateAgo(\DateTime $dt) {
+        return DateUtil::ago($dt);
+    }
+
+
+}
